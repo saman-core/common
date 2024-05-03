19bd9cb666bf4c5ec9e953df42db83fe0c0fcc24
cfernandez <Ycuervo.659>
2024-05-03 11:09:01 -0400

    add interfaces to encrypt

--- src/main/java/io/samancore/common/transformer/Encrypt.java
+++ src/main/java/io/samancore/common/transformer/Encrypt.java
@@ -2,7 +2,7 @@ package io.samancore.common.transformer;
 
 public interface Encrypt {
 
-    Object encrypt(Object value);
+    byte[] encrypt(Object value);
 
-    Object decrypt(Object value);
+    String decrypt(byte[] value);
 }
