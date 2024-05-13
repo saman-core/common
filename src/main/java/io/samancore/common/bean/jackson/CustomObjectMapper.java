9ffc7a34c5fc868d5a1a22da3c999f68526c9be1
cfernandez <Ycuervo.659>
2024-05-13 11:25:17 -0400

    change DateFormat to use FormIo Date Format

--- src/main/java/io/samancore/common/bean/jackson/CustomObjectMapper.java
+++ src/main/java/io/samancore/common/bean/jackson/CustomObjectMapper.java
@@ -4,18 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static io.samancore.common.SerializationConstant.DATE_FORMAT_ISO8601;
-
 public class CustomObjectMapper {
+
+    private static final SimpleDateFormat DATE_FORMAT_FORMIO = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+
     private CustomObjectMapper() {
     }
 
     public static ObjectMapper changeFeature(ObjectMapper mapper) {
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        mapper.setDateFormat(DATE_FORMAT_ISO8601);
+        mapper.setDateFormat(DATE_FORMAT_FORMIO);
         SimpleModule dateModule = new SimpleModule();
         dateModule.addSerializer(Date.class, new DateSerializer());
         mapper.registerModule(dateModule);
