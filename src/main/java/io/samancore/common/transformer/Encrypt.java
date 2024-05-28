468313fa03508a0d649afbd72c417900c5d30f6e
cfernandez <Ycuervo.659>
2024-05-28 13:25:22 -0400

    revert change in Encrypt interface,

--- src/main/java/io/samancore/common/transformer/Encrypt.java
+++ src/main/java/io/samancore/common/transformer/Encrypt.java
@@ -5,8 +5,4 @@ public interface Encrypt {
     byte[] encrypt(String value);
 
     String decrypt(byte[] value);
-
-    byte[] encryptAsymmetric(String value);
-
-    String decryptAsymmetric(byte[] value);
 }
