0068eec462e342a2302a847636d74808d79982f3
Hernael <hperer@movealong.tech>
2024-10-02 21:53:25 -0400

    upgrade Java version

--- src/main/java/io/samancore/common/util/ConditionsUtil.java
+++ src/main/java/io/samancore/common/util/ConditionsUtil.java
@@ -2,13 +2,13 @@ package io.samancore.common.util;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.util.internal.StringUtil;
 import io.samancore.common.bean.jackson.CustomObjectMapper;
 import io.samancore.common.error.exception.BusinessException;
 import io.samancore.common.error.message.BusinessExceptionsEnum;
 import io.samancore.common.model.condition.Condition;
 import io.samancore.common.model.condition.ConditionRequest;
 import io.samancore.common.model.condition.ConditionType;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -29,7 +29,7 @@ public class ConditionsUtil {
     }
 
     public static Map<String, Object> getModelMapUpdatedWithConditions(Map<String, Object> modelMap, List<Condition> conditions) {
-        var validateError = conditions.stream().filter(condition -> condition.getConditionType().equals(ConditionType.VALIDATE) && condition.getValue() != null && !StringUtil.isNullOrEmpty(condition.getValue().toString())).toList();
+        var validateError = conditions.stream().filter(condition -> condition.getConditionType().equals(ConditionType.VALIDATE) && condition.getValue() != null && !StringUtils.isEmpty(condition.getValue().toString())).toList();
         if (!validateError.isEmpty()) {
             var allProperties = validateError.stream().map(condition -> condition.getValue().toString()).collect(Collectors.joining(","));
             throw new BusinessException(BusinessExceptionsEnum.CONDITION_VALIDATE_ERROR, List.of(allProperties));
